e22669a80931f87b18adf971af74128ae7368e7f
ultrapants <gottlieb@psych.ubc.ca>
2013-10-10 13:16:03 -0700

    Home6

--- registration/views.py
+++ registration/views.py
@@ -34,7 +34,7 @@ def home_redirect(request):
 	if request.user.is_authenticated():
 		return HttpResponseRedirect('matches/view-matches')
 	else:
-		return HttpResponse('accounts/home')
+		return HttpResponse('registration/home.html')
 
 # View: registraion
 # Allows the user to create an INACTIVE User account and user profile (a Clinician instance)
