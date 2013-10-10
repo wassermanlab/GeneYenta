6a07b82dba3065b74aaf6788982008bb36347e26
ultrapants <gottlieb@psych.ubc.ca>
2013-10-10 13:19:45 -0700

    Revert "HomePage2"
    This reverts commit 63be04ca585b16255eebcb8e585e9ae48875407f.

--- registration/views.py
+++ registration/views.py
@@ -34,7 +34,7 @@ def home_redirect(request):
 	if request.user.is_authenticated():
 		return HttpResponseRedirect('matches/view-matches')
 	else:
-		return HttpResponseRedirect('accounts/home')
+		return HttpResponseRedirect('accounts/login')
 
 # View: registraion
 # Allows the user to create an INACTIVE User account and user profile (a Clinician instance)
