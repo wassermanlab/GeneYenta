d9e2a0ce5d14245ecce8888dfa8d0213de11f21e
JackWu <jack.wuweimin@gmail.com>
2014-04-05 11:17:03 -0700

    Updated gitignore

--- geneyenta/settings.py
+++ geneyenta/settings.py
@@ -25,8 +25,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'GeneYenta',                  # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'root',
+        
+        'USER': 'gyadmin',
+        'PASSWORD': 'gnytdmpw',
+        #'USER': 'root',
+        #'PASSWORD': 'root',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
